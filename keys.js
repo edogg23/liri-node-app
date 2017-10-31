040e43173ff3c82bbfb590552f5b9ea24fc5d1c6
Ernest C <Ernest C>
2017-10-31 18:28:02 -0400

    created a few more files

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'w1p0UeXSgG4c2l40CWaU8Kq6Q',
+  consumer_secret: 'qYpTmSVvw2uw6cCdTs0MqIevpFV95UC8cJL6df0dcXoHwfjbbQ',
+  access_token_key: '3322094081-j24I0TiaoqwxUZ6KPkx8b0dBa74pEKKv3kkUqTs',
+  access_token_secret: '40LUAI4NFmCzz77wsgCNUBxE1RRTW0KnIty9HujvAylIT',
 }
 
 module.exports = twitterKeys;
