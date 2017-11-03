16d9b29afc6a9f760d6d79a034caf6bc1c741911
Ernest C <Ernest C>
2017-11-02 23:29:35 -0400

    shows latest tweets and date created don't hv 20 tweets yet

--- liri.js
+++ liri.js
@@ -1,8 +1,61 @@
 var fs = require("fs");
 var twitterKeys = require("./keys.js");
+var Twitter = require("twitter");
 
-console.log(twitterKeys);
-console.log(twitterKeys.consumer_key);
+var params = {screen_name: 'edog231'};
+var client = new Twitter({
+	consumer_key: twitterKeys.consumer_key,
+	consumer_secret: twitterKeys.consumer_secret,
+	access_token_key: twitterKeys.access_token_key,
+	access_token_secret: twitterKeys.access_token_secret,
+});
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	if(error) {
+		console.log(error);
+	} else {
+		var tweetArr = [];
+		// console.log("tweets: " + tweets[2].text);
+		// console.log("tweets Created at: " + tweets[2].created_at);
+		for(var i = 0; i < tweets.length; i++) {
+			tweetArr.push({
+				"Latest Tweets: " : tweets[i].text,
+				"Created at: " : tweets[i].created_at,				
+			});
+		}
+		console.log(tweetArr);
+	}
+ })
+
+// This is all for the Twit package. 
+// var Twit = require("twit");
+// var T = new Twit({
+// 	consumer_key: twitterKeys.consumer_key,
+// 	consumer_secret: twitterKeys.consumer_secret,
+// 	access_token: twitterKeys.access_token_key,
+// 	access_token_secret: twitterKeys.access_token_secret,
+// 	timeout_ms: 60*1000,
+// });
+
+// This posts to Twitter
+// client.post('statuses/update', {status: 'tweet from node using twitter npm package'}, function(err, data, response) {
+// 	console.log(data);
+// })
+
+client.get('search/tweets', { q: 'tweet from node using twitter npm package', count:20 }, function(err, data, response) {
+	// console.log(data);
+})
+console.log("twitterKeys: " + twitterKeys);
+
+// T.get('search/tweets', params,searchedData);
+
+function searchedData(err, data, response) {
+	// console.log(data);
+}
+
+// console.log()
+// console.log(twitterKeys.consumer_key);
+
 // this does the same thing as console.log(twitterKeys);
 // fs.readFile("keys.js", "utf8", function(error, data) {
 // 	if(error) {
